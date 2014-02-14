d83d520fb5967f31fb0ebc34a28e2a3bf407486b
Simon Mihevc <simon@simon-zemanta.zemanta.com>
2014-02-14 10:09:37 +0100

    Proper title added.

--- service.py
+++ service.py
@@ -11,10 +11,13 @@ def hello():
 def twitter():
 	keywords = ["zemanta"]
 	twitter_filter = "+OR+".join(["%23s" + keyword for keyword in keywords])
+	name = "Zemanta twiti"
 	twitter_feed = """
-	<a class="twitter-timeline" href="https://twitter.com/search?q=%s" data-widget-id="434246345767936000">Tweets concernant "#sawickipedia OR #tomaz"</a>
+	<div>
+	<a class="twitter-timeline" href="https://twitter.com/search?q=%s" data-widget-id="434246345767936000">%s</a>
 	<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
-	""" % twitter_filter
+	</div>
+	""" % (twitter_filter, name)
 	return "<html><body>" + twitter_feed + "</body></html>"
 
 
