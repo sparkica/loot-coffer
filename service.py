759a6011a1c55cdb72355b5a740239b8ab325f6a
Mateja Verlic <mateja.verlic@zemanta.com>
2014-02-14 11:55:52 +0100

    Fixed chars

--- service.py
+++ service.py
@@ -82,12 +82,12 @@ def google():
 	from google.appengine.api import search
 
 	# a query string like this comes from the client
-	querystring = “stories”
+	querystring = "stories"
 	try:
-	 	index = search.Index(INDEX_NAME)
-	  	search_query = search.Query(
-		  	query_string=querystring,
-	      	options=search.QueryOptions(limit=doc_limit))
+		index = search.Index(INDEX_NAME)
+		search_query = search.Query(
+			query_string=querystring,
+			options=search.QueryOptions(limit=doc_limit))
 		search_results = index.search(search_query)
 	except search.Error:
 		pass
