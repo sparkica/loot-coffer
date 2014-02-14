4c86db1aa40dfa159cab41355fdfd26564c728a5
Mateja Verlic <mateja.verlic@zemanta.com>
2014-02-14 10:15:41 +0100

    Basic wp stats

--- service.py
+++ service.py
@@ -1,9 +1,42 @@
-from flask import Flask
+import urllib
+import simplejson
+
+from flask import Flask, request, jsonify, json
+from flask import render_template, redirect
 app = Flask(__name__)
 
+
+WP_API_GATEWAY = "http://wpapi.org/api/plugin/"
+
+
+@app.route("/wp/")
+def wp_stats(names=None, methods=['GET', 'POST']):
+
+	stats = {}
+	if request.method == 'GET':
+		plug_slug = request.args.get('plugin')
+
+		if plug_slug:
+			#http://wpapi.org/api/type/slug.format
+			json_res = urllib.urlopen(WP_API_GATEWAY + plug_slug + '.json').read()
+			res = simplejson.loads(json_res)
+			stats = [{'name' : res['name'],
+					'slug' : plug_slug,
+					'rating': res['rating'],
+					'avg_downloads' : res['average_downloads'],
+					'hits': res['hits']
+					}]
+		else:
+			print "No plugin slug"
+	else:
+		stats = None
+
+	return render_template('wordpress.html', name=plug_slug, stats=stats)
+
+
 @app.route("/")
-def hello():
-	return "Hello harvesters!"
+def index():
+	return render_template('index.html')
 
 if __name__ == "__main__":
-	app.run()
+	app.run(debug=True)
